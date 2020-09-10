e2ecb7877691380195a8d40ffc8f7d972d9d1c8c
David Ka <david.karnel@student.kyh.se>
2020-09-10 14:55:44 +0200

    testtest branch

--- src/com/company/PrintMean.java
+++ src/com/company/PrintMean.java
@@ -8,7 +8,7 @@ public class PrintMean {
 
         System.out.println("Mean of 1, 1, 3, 3: " + meanCalc(1, 1 , 3, 3));
         System.out.println("Mean of 1, 2, 3, 4: " + meanCalc(1, 2 , 3, 4));
-        System.out.println("Mean of 1, -2, 3, -4: " + meanCalc(1, -2 , 3, -4));
+        System.out.println("Mean of 1, -2, 3, -4: " + meanCalc(1, -2 , 15, -4));
         System.out.println("Mean of -1, -11, 24, 2: " + meanCalc(-1, -11, 24, 2));
     }
     public static String meanCalc(int a, int b, int c, int d) {
